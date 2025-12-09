e281ac1bdf71c83ace344e7f641c1f1044617166
Mamahaha2004 <wongjt-wp22@student.tarc.edu.my>
2025-12-09 15:25:45 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c3mis482oeia.us-east-1.rds.amazonaws.com"
+customuser = "aws_wongjuntong"
+custompass = "Master123"
+customdb = "employee"
+custombucket = "wongjt-employee"
+customregion = "us-east-1"
+
